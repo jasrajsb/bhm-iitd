2f9c739615f2509d733f470c8174f66aeb7389d4
guptaanurag2106 <guptaanurag2106@gmail.com>
2021-12-04 18:57:52 +0530

    gallery comp use sample

--- bhm-site/src/App.js
+++ bhm-site/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
-import Navbar from './components/navbar/component.js' 
-import Hero from './components/hero/component.js' 
 import AboutUs from './components/about-us/component';
-import TeamStucture from './components/team-structure/component';
+import Calendar from './components/calendar/component';
 import Chiefs from './components/chiefs/component';
 import Footer from './components/footer/component';
-import PopUp from './components/pop-up/component';
-import Calendar from './components/calendar/component';
+import Gallery from './components/gallery/component';
+import Hero from './components/hero/component.js';
+import Navbar from './components/navbar/component.js';
 import Notices from './components/notices/component.js';
+import PopUp from './components/pop-up/component';
+import TeamStucture from './components/team-structure/component';
 function App() {
   return (
     <div className="App" id="container">
-        <Navbar/>
-        <Hero/>
-        <AboutUs/>
-        <TeamStucture/>
-        <Notices/>
-        <Calendar/>
-        <Chiefs/>
-        <Footer/>
-        <PopUp/>
+      <Navbar />
+      <Hero />
+      <AboutUs />
+      <TeamStucture />
+      <Notices />
+      <Calendar />
+      <Chiefs />
+      <Gallery hostel={"Zanskar"} />
+      <Footer />
+      <PopUp />
     </div>
   );
 }
