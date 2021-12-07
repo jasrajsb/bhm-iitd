b3121bf06957f7d1159e45814898a031ee9da683
SaurabhTiwari4093 <86095513+SaurabhTiwari4093@users.noreply.github.com>
2021-12-07 22:53:23 +0530

    Updated NavBar-Similar to BSW website

--- bhm-site/src/components/navbar/component.js
+++ bhm-site/src/components/navbar/component.js
@@ -93,7 +93,7 @@ function Navbar() {
             <a className="nav-link" target="_blank" rel="noreferrer"  href="https://docs.google.com/spreadsheets/d/1okeMA5gB62KvVrHQQuwZ4vNKg38NBybJE7F7h7HmBU4/edit#gid=0">Hostel Rules</a>
           </li>
         </ul>
-        <form className="d-inline-flex search-bar">
+        <form className="d-inline-flex search-bar bg-light">
           <button className="btn btn-outline" type="button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16" color="rgb(100,100,100)">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg></button>
