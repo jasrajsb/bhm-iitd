0daf5944fb5ff73292e7309810aaed8cc4474b00
guptaanurag2106 <guptaanurag2106@gmail.com>
2021-12-19 00:43:00 +0530

    example gallery usage

--- bhm-site/src/pages/homepage.js
+++ bhm-site/src/pages/homepage.js
@@ -1,20 +1,22 @@
-import Hero from '../components/hero/component';
 import AboutUs from '../components/about-us/component';
-import TeamStucture from '../components/team-structure/component';
-import Notices from '../components/notices/component';
 import Calendar from '../components/calendar/component';
-import PopUp from '../components/pop-up/component';
 import Chiefs from '../components/chiefs/component';
+import Gallery from '../components/gallery/component';
+import _template_for_gallery from '../components/gallery/_template_for_gallery';
+import Hero from '../components/hero/component';
+import Notices from '../components/notices/component';
+import PopUp from '../components/pop-up/component';
 
 const Homepage = function () {
     return <div>
         <Hero />
         <AboutUs />
-        <TeamStucture />
+        {/* <TeamStucture /> */}
         <Notices />
         <Calendar />
         <Chiefs />
         <PopUp />
+        <Gallery images_json={_template_for_gallery} />
     </div>
 }
 
